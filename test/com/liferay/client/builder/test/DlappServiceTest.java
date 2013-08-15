9175ec99184dceb9a1b4d0c9bbe196fd3b2ce92f
Bruno Farache <bruno.farache@liferay.com>
2013-08-15 18:42:05 -0300

    MOBILE-479 - Create unit tests

---
+++ test/com/liferay/client/builder/test/DlappServiceTest.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.client.builder.test;
+
+import com.liferay.client.v62.dlapp.DlappService;
+
+import java.io.IOException;
+
+import org.json.JSONObject;
+
+import org.junit.Test;
+
+/**
+ * @author Bruno Farache
+ */
+public class DlappServiceTest extends BaseTest {
+
+	public DlappServiceTest() throws IOException {
+		super();
+	}
+
+	@Test
+	public void addFolder() throws Exception {
+		DlappService service = new DlappService(context);
+
+		long repositoryId = Long.valueOf(props.getProperty("repositoryId"));
+		long parentFolderId = 0;
+		String name = "test";
+		String description = "";
+		JSONObject serviceContext = null;
+
+		service.addFolder(
+			repositoryId, parentFolderId, name, description, serviceContext);
+	}
+
+}
