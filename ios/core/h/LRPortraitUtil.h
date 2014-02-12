b803e48f5e0549b2c8264403f0f01c2c97210476
Bruno Farache <bruno.farache@liferay.com>
2014-02-12 16:14:15 -0300

    Adding portrait utility for iOS

---
+++ ios/core/h/LRPortraitUtil.h
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+#import <CommonCrypto/CommonDigest.h>
+#import "LRHttpUtil.h"
+#import "LRSession.h"
+
+/**
+ * @author Bruno Farache
+ */
+@interface LRPortraitUtil : NSObject
+
++ (NSString *)getPortraitURL:(LRSession *)session male:(BOOL)male
+	portraitId:(long long)portraitId;
+
++ (NSString *)getPortraitURL:(LRSession *)session male:(BOOL)male
+	portraitId:(long long)portraitId uuid:(NSString *)uuid;
+
+@end
---
+++ ios/core/m/LRPortraitUtil.m
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+#import "LRPortraitUtil.h"
+
+/**
+ * @author Bruno Farache
+ */
+@implementation LRPortraitUtil
+
++ (NSString *)getPortraitURL:(LRSession *)session male:(BOOL)male
+		portraitId:(long long)portraitId {
+
+	return [self getPortraitURL:session male:male portraitId:portraitId
+		uuid:nil];
+}
+
++ (NSString *)getPortraitURL:(LRSession *)session male:(BOOL)male
+		portraitId:(long long)portraitId uuid:(NSString *)uuid {
+
+	NSString *gender = male ? @"male" : @"female";
+
+	NSString *portraitURL =
+		[NSString stringWithFormat:@"%@/image/user_%@_portrait?img_id=%lld",
+			session.server, gender, portraitId];
+
+	NSError *error;
+	int version = [LRHttpUtil getPortalVersion:session error:&error];
+
+	if (!error && version > V_6_2) {
+		portraitURL = [self appendToken:portraitURL uuid:uuid];
+	}
+
+	return portraitURL;
+}
+
++ (NSString *)appendToken:(NSString *)portraitURL uuid:(NSString *)uuid {
+	if (!uuid) {
+		return portraitURL;
+	}
+
+	NSString *token = [self sha1:uuid];
+
+	return [NSString stringWithFormat:@"%@&img_id_token=%@",
+		portraitURL, token];
+}
+
++ (NSString *)sha1:(NSString *)input {
+	unsigned char result[CC_SHA1_DIGEST_LENGTH];
+	const char *string = [input UTF8String];
+
+	CC_SHA1(string, strlen(string), result);
+
+	NSData *data = [[NSData alloc] initWithBytes:result length:(sizeof result)];
+	return [data base64Encoding];
+}
+
+@end
