7acfbbc58edddbb088ebfa892d2c317b31b11339
Gregory Amerson <gregory.amerson@liferay.com>
2014-02-19 17:57:35 +0800

    Fixed ClassCastException with PortletService.getWarPortlets() now return JSONArray instead of JSONObject

--- src/com/liferay/mobile/sdk/util/LanguageUtil.java
+++ src/com/liferay/mobile/sdk/util/LanguageUtil.java
@@ -72,6 +72,7 @@ public class LanguageUtil {
 	public boolean isArray(String type) {
 		if (type.endsWith("[]") || type.equals("object<list>") ||
 			type.equals("object<com.liferay.portal.kernel.json.JSONArray>") ||
+			type.startsWith("java.util.Collection") ||
 			type.startsWith("list")) {
 
 			return true;
