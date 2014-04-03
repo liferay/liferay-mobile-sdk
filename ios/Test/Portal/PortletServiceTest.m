214b64c5b7ddaa69b81756ec620dd70d5db52c81
Bruno Farache <bruno.farache@liferay.com>
2014-04-03 17:35:10 +0200

    Adding copyright header to tests

--- ios/Test/Portal/PortletServiceTest.m
+++ ios/Test/Portal/PortletServiceTest.m
@@ -1,13 +1,23 @@
-//
-//  PortletServiceTest.m
-//  Liferay iOS SDK
-//
-//  Bruno Farache
-//
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
 
 #import "LRPortletService_v62.h"
 #import "PortletServiceTest.h"
 
+/**
+ * @author Bruno Farache
+ */
 @implementation PortletServiceTest
 
 - (void)testGetUserSitesGroups {
