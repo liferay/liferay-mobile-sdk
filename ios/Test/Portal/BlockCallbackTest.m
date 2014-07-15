ccf539c63dc1062a98b6d3365d5e34e1c628ec42
Bruno Farache <bruno.farache@liferay.com>
2014-07-14 23:59:00 -0300

    MOBILESDK-26 - Accept blocks as callbacks

---
+++ ios/Test/Portal/BlockCallbackTest.m
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-2014 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+#import "GroupServiceTest.h"
+#import "LRBlockCallback.h"
+#import "LRGroupService_v62.h"
+#import "TRVSMonitor.h"
+
+/**
+ * @author Bruno Farache
+ */
+@interface BlockCallbackTest : GroupServiceTest
+
+@property (nonatomic, strong) NSError *error;
+@property (nonatomic, strong) NSArray *groups;
+@property (nonatomic, strong) TRVSMonitor *monitor;
+
+@end
+
+@implementation BlockCallbackTest
+
+- (void)testGetUserSites {
+	self.monitor = [TRVSMonitor monitor];
+
+	LRSession *session = [[LRSession alloc] initWithSession:self.session];
+
+	id<LRCallback> callback = [[LRBlockCallback alloc]
+		initWithSuccess:^(id groups) {
+			[self setGroups:groups];
+			[self.monitor signal];
+		}
+		failure:^(NSError *error) {
+			[self setError:error];
+			[self.monitor signal];
+		}
+	];
+
+	[session setCallback:callback];
+
+	LRGroupService_v62 *service = [[LRGroupService_v62 alloc]
+		initWithSession:session];
+
+	NSError *error;
+	[service getUserSites:&error];
+	[self.monitor wait];
+
+	XCTAssertNil(error);
+
+	[self assert:self.groups error:self.error];
+}
+
+@end
