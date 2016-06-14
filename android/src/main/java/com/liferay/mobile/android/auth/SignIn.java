09df15937677a118807ab7a58532ccbc771ab090
Javier Gamarra <nhpatt@gmail.com>
2016-06-14 11:41:00 +0200

    add sign-in method that returns a session with cookie authentication

--- android/src/main/java/com/liferay/mobile/android/auth/SignIn.java
+++ android/src/main/java/com/liferay/mobile/android/auth/SignIn.java
@@ -15,20 +15,108 @@
 package com.liferay.mobile.android.auth;
 
 import com.liferay.mobile.android.auth.basic.BasicAuthentication;
+import com.liferay.mobile.android.auth.basic.CookieAuthentication;
+import com.liferay.mobile.android.callback.BaseCallback;
+import com.liferay.mobile.android.callback.Callback;
 import com.liferay.mobile.android.callback.typed.JSONArrayCallback;
 import com.liferay.mobile.android.callback.typed.JSONObjectCallback;
+import com.liferay.mobile.android.exception.AuthenticationException;
+import com.liferay.mobile.android.http.Method;
+import com.liferay.mobile.android.http.Request;
+import com.liferay.mobile.android.http.Response;
+import com.liferay.mobile.android.http.client.OkHttpClientImpl;
 import com.liferay.mobile.android.service.Session;
 import com.liferay.mobile.android.service.SessionImpl;
 import com.liferay.mobile.android.v7.group.GroupService;
 import com.liferay.mobile.android.v7.user.UserService;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.RequestBody;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.CookieManager;
+import java.net.CookiePolicy;
+import java.net.CookieStore;
+import java.net.HttpCookie;
+import java.util.HashMap;
+import java.util.Map;
+
+import okio.Buffer;
 
 /**
  * @author Bruno Farache
  */
 public class SignIn {
+
+	public static void signInWithCookie(Session session, final BaseCallback<Session> callback) {
+
+		try {
+			if (!(session.getAuthentication() instanceof BasicAuthentication)) {
+				throw new Exception(
+					"Can't sign in if authentication implementation is not BasicAuthentication");
+			}
+
+			BasicAuthentication authentication = (BasicAuthentication) session.getAuthentication();
+			String body = getStringBody(authentication);
+
+			OkHttpClientImpl okHttpClient = new OkHttpClientImpl();
+			final CookieManager cookieManager = new CookieManager();
+			cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
+			okHttpClient.setCookieHandler(cookieManager);
+
+			Map<String, String> headers = new HashMap<>(session.getHeaders());
+			headers.put("Cookie", "COOKIE_SUPPORT=true;");
+			session.setHeaders(headers);
+
+			Request request = new Request(
+				authentication, Method.POST, session.getHeaders(),
+				getLoginUrl(session.getServer()), body, session.getConnectionTimeout(),
+				getCookieCallback(session.getServer(), callback, cookieManager), null,
+				"application/x-www-form-urlencoded", true);
+
+			okHttpClient.send(request);
+		}
+		catch (Exception e) {
+			callback.onFailure(e);
+		}
+	}
+
+	private static Callback getCookieCallback(final String server,
+	                                          final BaseCallback<Session> callback,
+	                                          final CookieManager cookieManager) {
+		return new Callback() {
+			@Override
+			public void inBackground(Response response) {
+				try {
+					String body = response.getBody();
+					Integer position = body.indexOf(AUTH_TOKEN) + AUTH_TOKEN.length();
+					String authToken = body.substring(position, position + TOKEN_LENGTH);
+
+					String cookieHeader = getHttpCookies(cookieManager.getCookieStore());
+
+					if (!cookieHeader.isEmpty()) {
+						Authentication authentication =
+							new CookieAuthentication(authToken, cookieHeader);
+						Session session = new SessionImpl(server, authentication);
+						callback.onSuccess(session);
+					}
+					else {
+						callback.onFailure(new AuthenticationException("Cookie invalid or empty"));
+					}
+				}
+				catch (Exception e) {
+					callback.onFailure(e);
+				}
+			}
+
+			@Override
+			public void doFailure(Exception exception) {
+				callback.onFailure(exception);
+			}
+		};
+	}
 
 	public static void signIn(
 		final Session session, final JSONObjectCallback callback,
@@ -110,7 +198,38 @@ public class SignIn {
 					"BasicAuthentication");
 		}
 
-		return ((BasicAuthentication)auth).getUsername();
+		return ((BasicAuthentication) auth).getUsername();
 	}
 
+	private static String getHttpCookies(CookieStore cookieStore) {
+		String cookies = "";
+		for (HttpCookie cookie : cookieStore.getCookies()) {
+			cookies += cookie + ";";
+		}
+		return cookies;
+	}
+
+	private static String getLoginUrl(String server) {
+		if (!server.endsWith("/")) {
+			server += "/";
+		}
+		return server + "c/portal/login";
+	}
+
+	private static String getStringBody(BasicAuthentication basicAuthentication)
+		throws IOException {
+
+		RequestBody formBody = new FormEncodingBuilder()
+			.add("login", basicAuthentication.getUsername())
+			.add("password", basicAuthentication.getPassword())
+			.build();
+
+		Buffer buffer = new Buffer();
+		formBody.writeTo(buffer);
+		return buffer.readUtf8();
+	}
+
+	private static final int TOKEN_LENGTH = 8;
+	private static final String AUTH_TOKEN = "Liferay.authToken=\"";
+	
 }
