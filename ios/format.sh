4ed6a72b2957437a33da04e0c1ce5464f8c5785c
brunofarache <bruno.farache@liferay.com>
2016-07-14 11:52:42 -0300

    Leave only Android SDK related files

--- ios/format.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-if [ -z "$1" ]; then
-	for file in $(find . -name "*.[hm]" -not -path "./Source/Service/*"); do
-		uncrustify -l OC -c uncrustify.cfg --no-backup $file
-	done
-else
-	uncrustify -l OC -c uncrustify.cfg --no-backup $1
-fi
