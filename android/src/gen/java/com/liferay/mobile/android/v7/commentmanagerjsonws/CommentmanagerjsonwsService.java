fd6d3929808d605a2728b02b7b6c7b5717233cb8
Alejandro Hernández <info@alejandrohdezma.com>
2016-07-18 09:53:11 +0200

    update comment jsonws services

--- android/src/gen/java/com/liferay/mobile/android/v7/commentmanagerjsonws/CommentmanagerjsonwsService.java
+++ android/src/gen/java/com/liferay/mobile/android/v7/commentmanagerjsonws/CommentmanagerjsonwsService.java
@@ -32,6 +32,31 @@ public class CommentmanagerjsonwsService extends BaseService {
 		super(session);
 	}
 
+	public JSONArray getComments(long commentId, int start, int end) throws Exception {
+		JSONObject _command = new JSONObject();
+
+		try {
+			JSONObject _params = new JSONObject();
+
+			_params.put("commentId", commentId);
+			_params.put("start", start);
+			_params.put("end", end);
+
+			_command.put("/comment.commentmanagerjsonws/get-comments", _params);
+		}
+		catch (JSONException _je) {
+			throw new Exception(_je);
+		}
+
+		JSONArray _result = session.invoke(_command);
+
+		if (_result == null) {
+			return null;
+		}
+
+		return _result.getJSONArray(0);
+	}
+
 	public JSONArray getComments(long groupId, String className, long classPK, int start, int end) throws Exception {
 		JSONObject _command = new JSONObject();
 
@@ -102,6 +127,71 @@ public class CommentmanagerjsonwsService extends BaseService {
 		session.invoke(_command);
 	}
 
+	public Long updateComment(String className, long classPK, long commentId, String subject, String body) throws Exception {
+		JSONObject _command = new JSONObject();
+
+		try {
+			JSONObject _params = new JSONObject();
+
+			_params.put("className", checkNull(className));
+			_params.put("classPK", classPK);
+			_params.put("commentId", commentId);
+			_params.put("subject", checkNull(subject));
+			_params.put("body", checkNull(body));
+
+			_command.put("/comment.commentmanagerjsonws/update-comment", _params);
+		}
+		catch (JSONException _je) {
+			throw new Exception(_je);
+		}
+
+		JSONArray _result = session.invoke(_command);
+
+		if (_result == null) {
+			return null;
+		}
+
+		return _result.getLong(0);
+	}
+
+	public void subscribeDiscussion(long groupId, String className, long classPK) throws Exception {
+		JSONObject _command = new JSONObject();
+
+		try {
+			JSONObject _params = new JSONObject();
+
+			_params.put("groupId", groupId);
+			_params.put("className", checkNull(className));
+			_params.put("classPK", classPK);
+
+			_command.put("/comment.commentmanagerjsonws/subscribe-discussion", _params);
+		}
+		catch (JSONException _je) {
+			throw new Exception(_je);
+		}
+
+		session.invoke(_command);
+	}
+
+	public void unsubscribeDiscussion(long groupId, String className, long classPK) throws Exception {
+		JSONObject _command = new JSONObject();
+
+		try {
+			JSONObject _params = new JSONObject();
+
+			_params.put("groupId", groupId);
+			_params.put("className", checkNull(className));
+			_params.put("classPK", classPK);
+
+			_command.put("/comment.commentmanagerjsonws/unsubscribe-discussion", _params);
+		}
+		catch (JSONException _je) {
+			throw new Exception(_je);
+		}
+
+		session.invoke(_command);
+	}
+
 	public Integer getCommentsCount(long groupId, String className, long classPK) throws Exception {
 		JSONObject _command = new JSONObject();
 
@@ -150,71 +240,6 @@ public class CommentmanagerjsonwsService extends BaseService {
 		}
 
 		return _result.getBoolean(0);
-	}
-
-	public void subscribeDiscussion(long groupId, String className, long classPK) throws Exception {
-		JSONObject _command = new JSONObject();
-
-		try {
-			JSONObject _params = new JSONObject();
-
-			_params.put("groupId", groupId);
-			_params.put("className", checkNull(className));
-			_params.put("classPK", classPK);
-
-			_command.put("/comment.commentmanagerjsonws/subscribe-discussion", _params);
-		}
-		catch (JSONException _je) {
-			throw new Exception(_je);
-		}
-
-		session.invoke(_command);
-	}
-
-	public void unsubscribeDiscussion(long groupId, String className, long classPK) throws Exception {
-		JSONObject _command = new JSONObject();
-
-		try {
-			JSONObject _params = new JSONObject();
-
-			_params.put("groupId", groupId);
-			_params.put("className", checkNull(className));
-			_params.put("classPK", classPK);
-
-			_command.put("/comment.commentmanagerjsonws/unsubscribe-discussion", _params);
-		}
-		catch (JSONException _je) {
-			throw new Exception(_je);
-		}
-
-		session.invoke(_command);
-	}
-
-	public Long updateComment(String className, long classPK, long commentId, String subject, String body) throws Exception {
-		JSONObject _command = new JSONObject();
-
-		try {
-			JSONObject _params = new JSONObject();
-
-			_params.put("className", checkNull(className));
-			_params.put("classPK", classPK);
-			_params.put("commentId", commentId);
-			_params.put("subject", checkNull(subject));
-			_params.put("body", checkNull(body));
-
-			_command.put("/comment.commentmanagerjsonws/update-comment", _params);
-		}
-		catch (JSONException _je) {
-			throw new Exception(_je);
-		}
-
-		JSONArray _result = session.invoke(_command);
-
-		if (_result == null) {
-			return null;
-		}
-
-		return _result.getLong(0);
 	}
 
 }
