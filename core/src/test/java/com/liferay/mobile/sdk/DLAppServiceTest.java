97b16be0d568635bddaccf31107fddeb2726fed1
brunofarache <bruno.farache@liferay.com>
2016-07-25 18:12:47 -0300

    Error has changed message

--- core/src/test/java/com/liferay/mobile/sdk/DLAppServiceTest.java
+++ core/src/test/java/com/liferay/mobile/sdk/DLAppServiceTest.java
@@ -131,7 +131,8 @@ public class DLAppServiceTest extends BaseTest {
 		}
 		catch (ServerException se) {
 			String message = se.getMessage();
-			assertTrue(message.startsWith("No DLFolder exists with the key"));
+			assertTrue(
+				message.startsWith("Request failed. Response code: 500"));
 		}
 	}
 
