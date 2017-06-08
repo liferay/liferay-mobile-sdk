67284e203c615f3f3eea3055c31a488bcd5d341f
Victor Galan <victor.galan@liferay.com>
2017-06-08 18:06:43 +0200

    Expose private method in tests

---
+++ ios/Test/LRCookieSignIn+Test.h
@@ -0,0 +1,17 @@
+//
+//  LRCookieSignIn+Test.h
+//  Liferay iOS SDK
+//
+//  Created by Victor Galán on 08/06/2017.
+//  Copyright © 2017 Liferay Inc. All rights reserved.
+//
+
+#import "LRCookieSignIn.h"
+
+
+@interface LRCookieSignIn (Test)
+
+
+- (NSString *)_getAuthToken:(NSString *)html;
+
+@end
