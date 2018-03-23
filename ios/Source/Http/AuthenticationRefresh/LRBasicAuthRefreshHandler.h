0111d0f16120fcb07a77deb2d9867e1f5150bd69
Victor Galan <victor.galan@liferay.com>
2018-03-23 13:51:29 +0100

    Create Basic authentication refresh handler

---
+++ ios/Source/Http/AuthenticationRefresh/LRBasicAuthRefreshHandler.h
@@ -0,0 +1,15 @@
+//
+//  BasicAuthRefreshHandler.h
+//  Liferay iOS SDK
+//
+//  Created by Victor Galan on 14/03/2018.
+//  Copyright © 2018 Liferay Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AFNetworking/AFNetworking.h>
+#import "LRAuthenticationRefreshHandler.h"
+
+@interface LRBasicAuthRefreshHandler : NSObject <LRAuthenticationRefreshHandler>
+
+@end
