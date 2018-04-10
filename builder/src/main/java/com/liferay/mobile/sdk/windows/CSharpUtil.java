007d9c7de307ff43849f77fca8b0b634b5b1341c
Andrea Di Giorgi <andrea.digiorgi@liferay.com>
2018-04-10 15:11:31 +0200

    Update windows sdk builder files to the last version

--- builder/src/main/java/com/liferay/mobile/sdk/windows/CSharpUtil.java
+++ builder/src/main/java/com/liferay/mobile/sdk/windows/CSharpUtil.java
@@ -1,15 +1,23 @@
 /**
- * Copyright (c) 2014 Andrea Di Giorgi. All rights reserved.
+ * Copyright (c) 2014-present Andrea Di Giorgi
  *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 package com.liferay.mobile.sdk.windows;
@@ -30,7 +38,7 @@ public class CSharpUtil extends LanguageUtil {
 	public static final String IDICTIONARY = "IDictionary<string, object>";
 
 	public static final String IDICTIONARY_STRING =
-		"IDictionary<string, string>";
+			"IDictionary<string, string>";
 
 	public static final String IENUMERABLE = "IEnumerable";
 
@@ -60,10 +68,10 @@ public class CSharpUtil extends LanguageUtil {
 		}
 
 		if (!ienumerableCastType.equals(BOOLEAN) &&
-			!ienumerableCastType.equals(DOUBLE) &&
-			!ienumerableCastType.equals(INT) &&
-			!ienumerableCastType.equals(LONG) &&
-			!ienumerableCastType.equals(STRING)) {
+				!ienumerableCastType.equals(DOUBLE) &&
+				!ienumerableCastType.equals(INT) &&
+				!ienumerableCastType.equals(LONG) &&
+				!ienumerableCastType.equals(STRING)) {
 
 			return _BLANK;
 		}
@@ -118,8 +126,8 @@ public class CSharpUtil extends LanguageUtil {
 
 	public String getType(String type) {
 		if (type.equals(BYTE_ARRAY) || type.equals(DOUBLE) ||
-			type.equals(INT) || type.equals(LONG) || type.equals(STRING) ||
-			type.equals(VOID)) {
+				type.equals(INT) || type.equals(LONG) || type.equals(STRING) ||
+				type.equals(VOID)) {
 
 			return type;
 		}
--- builder/src/main/java/com/liferay/mobile/sdk/windows/WindowsSDKBuilder.java
+++ builder/src/main/java/com/liferay/mobile/sdk/windows/WindowsSDKBuilder.java
@@ -1,15 +1,23 @@
 /**
- * Copyright (c) 2014 Andrea Di Giorgi. All rights reserved.
+ * Copyright (c) 2014-present Andrea Di Giorgi
  *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 package com.liferay.mobile.sdk.windows;
@@ -23,8 +31,22 @@ import com.liferay.mobile.sdk.util.Validator;
 import com.liferay.mobile.sdk.velocity.VelocityUtil;
 
 import java.io.File;
-import java.util.List;
+import java.io.IOException;
+import java.io.InputStream;
 
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.tools.generic.EscapeTool;
 
@@ -36,20 +58,117 @@ public class WindowsSDKBuilder extends BaseBuilder {
 	@Override
 	public void build(
 			Discovery discovery, List<Action> actions, String packageName,
-			int version, String filter, String destination) throws Exception {
+			int version, String filter, String destination)
+			throws Exception {
+
+		copyResource("windows", destination);
+		generateService(
+				discovery, actions, packageName, version, filter, destination);
+	}
+
+	protected void copyJarResource(
+			JarURLConnection jarConnection, File destinationDir)
+			throws IOException {
+
+		String jarConnectionEntryName = jarConnection.getEntryName();
+		JarFile jarFile = jarConnection.getJarFile();
+
+		Enumeration<JarEntry> enu = jarFile.entries();
+
+		while (enu.hasMoreElements()) {
+			JarEntry jarEntry = enu.nextElement();
+			String jarEntryName = jarEntry.getName();
+
+			if (jarEntryName.startsWith(jarConnectionEntryName)) {
+				String fileName = jarEntryName;
+
+				if (fileName.startsWith(jarConnectionEntryName)) {
+					fileName = fileName.substring(
+							jarConnectionEntryName.length());
+				}
+
+				File file = new File(destinationDir, fileName);
+
+				if (jarEntry.isDirectory()) {
+					file.mkdirs();
+				}
+				else {
+					InputStream is = null;
+
+					try {
+						is = jarFile.getInputStream(jarEntry);
+
+						FileUtils.copyInputStreamToFile(is, file);
+					}
+					finally {
+						IOUtils.closeQuietly(is);
+					}
+				}
+			}
+		}
+	}
+
+	protected void copyResource(String name, String destination)
+			throws IOException {
+
+		File destinationDir = new File(destination);
+
+		destinationDir = destinationDir.getAbsoluteFile();
+
+		URL sourceURL = getClass().getResource(CharPool.SLASH + name);
+		URLConnection sourceConnection = sourceURL.openConnection();
+
+		if (sourceConnection instanceof JarURLConnection) {
+			copyJarResource((JarURLConnection)sourceConnection, destinationDir);
+		}
+		else {
+			File sourceDir = new File(sourceURL.getPath());
+
+			FileUtils.copyDirectory(sourceDir, destinationDir);
+		}
+
+		Iterator<File> itr = FileUtils.iterateFiles(
+				destinationDir, new String[] {"copy"}, true);
+
+		while (itr.hasNext()) {
+			File file = itr.next();
+
+			String cleanPath = FilenameUtils.removeExtension(
+					file.getAbsolutePath());
+
+			File cleanFile = new File(cleanPath);
+
+			if (!cleanFile.exists()) {
+				FileUtils.moveFile(file, new File(cleanPath));
+			}
+			else {
+				file.delete();
+			}
+		}
+	}
+
+	protected void generateService(
+			Discovery discovery, List<Action> actions, String packageName,
+			int version, String filter, String destination)
+			throws Exception {
 
 		StringBuilder sb = new StringBuilder();
 
 		if (Validator.isNotNull(destination)) {
 			sb.append(destination);
-			sb.append(CharPool.SLASH);
+
+			if (!destination.endsWith("/")) {
+				sb.append(CharPool.SLASH);
+			}
 		}
 
 		sb.append("Liferay.SDK/Service");
 		destination = sb.toString();
 
+		packageName = "Liferay.SDK.Service";
+
 		VelocityContext context = getVelocityContext(
-				discovery, packageName, version, filter);
+				discovery, actions, packageName, version, filter);
 
 		String templatePath = "templates/windows/service.vm";
 		String filePath = getServiceFilePath(
@@ -59,8 +178,8 @@ public class WindowsSDKBuilder extends BaseBuilder {
 	}
 
 	protected String getServiceFilePath(
-		VelocityContext context, int version, String destination,
-		String filter) {
+			VelocityContext context, int version, String destination,
+			String filter) {
 
 		String className = (String)context.get(CLASS_NAME);
 		CSharpUtil cSharpUtil = (CSharpUtil)context.get(LANGUAGE_UTIL);
@@ -71,7 +190,6 @@ public class WindowsSDKBuilder extends BaseBuilder {
 		sb.append("/V");
 		sb.append(version);
 		sb.append(CharPool.SLASH);
-
 		sb.append(cSharpUtil.getServicePackageName(filter));
 		sb.append(CharPool.SLASH);
 
@@ -85,7 +203,8 @@ public class WindowsSDKBuilder extends BaseBuilder {
 	}
 
 	protected VelocityContext getVelocityContext(
-		Discovery discovery, String packageName, int version, String filter) {
+			Discovery discovery, List<Action> actions, String packageName,
+			int version, String filter) {
 
 		VelocityContext context = new VelocityContext();
 
@@ -100,17 +219,21 @@ public class WindowsSDKBuilder extends BaseBuilder {
 
 		packageName = sb.toString();
 
-		context.put(BYTE_ARRAY, LanguageUtil.BYTE_ARRAY);
 		context.put(CLASS_NAME, cSharpUtil.getServiceClassName(filter));
 		context.put(DISCOVERY, discovery);
+		context.put(ACTIONS, actions);
 		context.put(ESCAPE_TOOL, new EscapeTool());
 		context.put(JSON_OBJECT_WRAPPER, CSharpUtil.JSON_OBJECT_WRAPPER);
 		context.put(LANGUAGE_UTIL, cSharpUtil);
 		context.put(PACKAGE, packageName);
+		context.put(STREAM, CSharpUtil.STREAM);
 		context.put(VOID, LanguageUtil.VOID);
 
 		return context;
 	}
 
 	protected static final String PACKAGE = "package";
+
+	protected static final String STREAM = "STREAM";
+
 }
