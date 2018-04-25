cc9c6610fba1dde3fa1bf7834eef4ff4721c47b0
Victor Galan <victor.galan@liferay.com>
2018-04-25 17:18:16 +0200

    Add refresh handler for OAuth2 authentication

--- android/src/main/java/com/liferay/mobile/android/auth/refresh/AuthenticationRefreshHandlerFactory.java
+++ android/src/main/java/com/liferay/mobile/android/auth/refresh/AuthenticationRefreshHandlerFactory.java
@@ -2,12 +2,16 @@ package com.liferay.mobile.android.auth.refresh;
 
 import com.liferay.mobile.android.auth.Authentication;
 import com.liferay.mobile.android.auth.basic.CookieAuthentication;
+import com.liferay.mobile.android.auth.oauth2.OAuth2Authentication;
 
 public class AuthenticationRefreshHandlerFactory {
 
     public AuthenticationRefreshHandler refreshHandlerForAuthentication(Authentication authentication) {
         if (authentication instanceof CookieAuthentication) {
             return new CookieAuthenticationRefreshHandler();
+        }
+        else if (authentication instanceof OAuth2Authentication) {
+            return new OAuth2AuthenticationRefreshHandler();
         }
         else {
             return new BasicAuthenticationRefreshHandler();
---
+++ android/src/main/java/com/liferay/mobile/android/auth/refresh/OAuth2AuthenticationRefreshHandler.java
@@ -0,0 +1,75 @@
+package com.liferay.mobile.android.auth.refresh;
+
+import android.provider.Settings;
+import com.liferay.mobile.android.auth.CookieSignIn;
+import com.liferay.mobile.android.auth.OAuth2SignIn;
+import com.liferay.mobile.android.auth.SessionCallback;
+import com.liferay.mobile.android.auth.basic.CookieAuthentication;
+import com.liferay.mobile.android.auth.oauth2.OAuth2Authentication;
+import com.liferay.mobile.android.service.Session;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class OAuth2AuthenticationRefreshHandler implements AuthenticationRefreshHandler {
+
+	@Override
+	public Session refreshAuthentication(final Session session, final SessionCallback callback) throws Exception {
+
+		OAuth2Authentication authentication = (OAuth2Authentication) session.getAuthentication();
+		lock.lock();
+
+		if (shouldRefreshToken(authentication.getAccessTokenExpirationDate())) {
+			if (callback == null) {
+				try {
+					return OAuth2SignIn.refreshToken(session, authentication.getScope(),null);
+				} finally {
+					lock.unlock();
+				}
+			} else {
+
+				OAuth2SignIn.refreshToken(session, authentication.getScope(), new SessionCallback() {
+					@Override
+					public void onSuccess(Session oauth2Session) {
+						session.setAuthentication(oauth2Session.getAuthentication());
+
+						lock.unlock();
+
+						callback.onSuccess(session);
+					}
+
+					@Override
+					public void onFailure(Exception e) {
+						lock.unlock();
+
+						callback.onFailure(e);
+					}
+				});
+
+				return null;
+			}
+		} else {
+			lock.unlock();
+		}
+
+		if (callback != null) {
+			callback.onSuccess(session);
+
+			return null;
+		} else {
+			return session;
+		}
+	}
+
+	private boolean shouldRefreshToken(long accessTokenExpirationDate) {
+		long currentTime = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis());
+
+		long deltaTime = accessTokenExpirationDate - currentTime;
+
+		return deltaTime <= TOLERANCE;
+	}
+
+	protected static final Lock lock = new ReentrantLock();
+	private static final int TOLERANCE = 60;
+}
