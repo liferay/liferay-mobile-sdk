b930889ca46a7cc72de64c83199025b6981756ac
Victor Galan <victor.galan@liferay.com>
2019-10-01 12:13:37 +0200

    Use semaphore, as Reentrank lock requires the same thread to lock and unlock

--- android/android-sdk/src/main/java/com/liferay/mobile/android/auth/refresh/OAuth2AuthenticationRefreshHandler.java
+++ android/android-sdk/src/main/java/com/liferay/mobile/android/auth/refresh/OAuth2AuthenticationRefreshHandler.java
@@ -1,13 +1,11 @@
 package com.liferay.mobile.android.auth.refresh;
 
-import android.provider.Settings;
-import com.liferay.mobile.android.auth.CookieSignIn;
 import com.liferay.mobile.android.auth.OAuth2SignIn;
 import com.liferay.mobile.android.auth.SessionCallback;
-import com.liferay.mobile.android.auth.basic.CookieAuthentication;
 import com.liferay.mobile.android.auth.oauth2.OAuth2Authentication;
 import com.liferay.mobile.android.service.Session;
-import java.util.List;
+
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -18,14 +16,14 @@ public class OAuth2AuthenticationRefreshHandler implements AuthenticationRefresh
 	public Session refreshAuthentication(final Session session, final SessionCallback callback) throws Exception {
 
 		OAuth2Authentication authentication = (OAuth2Authentication) session.getAuthentication();
-		lock.lock();
+		semaphore.acquire();
 
 		if (shouldRefreshToken(authentication.getAccessTokenExpirationDate())) {
 			if (callback == null) {
 				try {
 					return OAuth2SignIn.refreshToken(session, authentication.getScope(),null);
 				} finally {
-					lock.unlock();
+					semaphore.release();
 				}
 			} else {
 
@@ -35,14 +33,14 @@ public class OAuth2AuthenticationRefreshHandler implements AuthenticationRefresh
 						public void onSuccess(Session oauth2Session) {
 							session.setAuthentication(oauth2Session.getAuthentication());
 
-							lock.unlock();
+							semaphore.release();
 
 							callback.onSuccess(session);
 						}
 
 						@Override
 						public void onFailure(Exception e) {
-							lock.unlock();
+							semaphore.release();
 
 							callback.onFailure(e);
 						}
@@ -50,13 +48,13 @@ public class OAuth2AuthenticationRefreshHandler implements AuthenticationRefresh
 				} catch (Exception e) {
 					callback.onFailure(e);
 				} finally {
-					lock.unlock();
+					semaphore.release();
 				}
 
 				return null;
 			}
 		} else {
-			lock.unlock();
+			semaphore.release();
 		}
 
 		if (callback != null) {
@@ -76,6 +74,6 @@ public class OAuth2AuthenticationRefreshHandler implements AuthenticationRefresh
 		return deltaTime <= TOLERANCE;
 	}
 
-	protected static final Lock lock = new ReentrantLock();
+	protected static final Semaphore semaphore = new Semaphore(1);
 	private static final int TOLERANCE = 60;
 }
